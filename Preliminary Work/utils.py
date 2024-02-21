f47840d659d8aad614eb37d488dcd4c5a8d9151d
Leonardo Cortes Zambrano <gleonardo.cortes@gmail.com>
2024-02-20 19:37:20 -0500

    Correction

--- Preliminary Work/utils.py
+++ Preliminary Work/utils.py
@@ -4,6 +4,7 @@ import sys
 import pyarrow.parquet as pq
 import pyarrow as pa
 import io
+import requests
 from datetime import datetime
 # Warnings ignore
 import warnings
@@ -11,7 +12,25 @@ warnings.filterwarnings("ignore")
 
 
 
+def read_parquet_file(file_path):
+    """
+    Read a Parquet file from the specified path.
 
+    Parameters:
+    file_path (str): The path to the Parquet file to be read.
+
+    Returns:
+    pyarrow.Table: The data table contained in the Parquet file.
+    """
+    try:
+        parquet_table = pq.read_table(file_path)
+        return parquet_table
+    except Exception as e:
+        print("Error reading Parquet file:", e)
+        return None
+    
+
+    
 def json_to_dataframe(main_folder_path):
     '''
     Function to read JSON files from a directory, process them,
@@ -366,22 +385,23 @@ def duplicates(df, column):
 #Function tu manage null values
 def replace_all_nulls(df):
     '''
-    Recieves a df as parameter and fill all the null values per column depending on their dType
+    Receives a df as parameter and fill all the null values per column depending on their dType
     '''
 
     for column in df.columns:
-        mask = df[column].notnull()
-        dtype = df[column][mask].apply(type).unique()
+        dtype = df[column].apply(type).unique()
 
-        if dtype[0] == str: 
-            df[column] = df[column].fillna('No data')
+        if None in dtype:  # Manejar valores None
+            df[column] = df[column].fillna('ND')
+        elif dtype[0] == str: 
+            df[column] = df[column].fillna('ND')
         elif dtype[0] == float:
             mean = df[column].mean()
             df[column] = df[column].fillna(mean)
         elif dtype[0] == list:
-            df[column] = df[column].fillna('No data')
-
-
+            df[column] = df[column].fillna('ND')
+            
+    return df
 
 #Function to merge Google Maps Data
 def merge_state_dataframes(state_directory, metadata_sites):
@@ -421,3 +441,129 @@ def merge_state_dataframes(state_directory, metadata_sites):
 
     # Return the merged DataFrame
     return merged_dataframe   
+
+
+def drop_columns(dataframe, columns_to_drop):
+    """
+    Drop the specified columns from a DataFrame.
+
+    Parameters:
+    dataframe (pd.DataFrame): The DataFrame from which columns will be dropped.
+    columns_to_drop (list): List of column names to be dropped.
+
+    Returns:
+    pd.DataFrame: The resulting DataFrame after dropping the columns.
+    """
+    try:
+        dataframe = dataframe.drop(columns=columns_to_drop)
+        return dataframe
+    except Exception as e:
+        print("Error dropping columns:", e)
+        return None
+    
+
+
+def explode_column(dataframe, column_name):
+    """
+    Explode a column in a DataFrame containing lists into multiple rows.
+
+    Parameters:
+    dataframe (pd.DataFrame): The DataFrame containing the column to be exploded.
+    column_name (str): The name of the column to be exploded.
+
+    Returns:
+    pd.DataFrame: The DataFrame with the specified column exploded into multiple rows.
+    """
+    try:
+        exploded_df = dataframe.explode(column_name)
+        return exploded_df
+    except Exception as e:
+        print("Error exploding column:", e)
+        return None
+    
+
+def group_column(dataframe, column_name):
+    """
+    Group a DataFrame by a specified column and aggregate values into a list.
+
+    Parameters:
+    dataframe (pd.DataFrame): The DataFrame containing the exploded column.
+    column_name (str): The name of the column to be grouped.
+
+    Returns:
+    pd.DataFrame: The DataFrame with the specified column grouped back into a list.
+    """
+    try:
+        # Agrupar la columna
+        grouped_column = dataframe.groupby(dataframe.index)[column_name].agg(list)
+
+        # Combinar el DataFrame original con la columna agrupada
+        grouped_df = pd.merge(dataframe.drop(columns=[column_name]).drop_duplicates(), grouped_column, left_index=True, right_index=True)
+        return grouped_df
+    except Exception as e:
+        print("Error grouping column:", e)
+        return None
+
+
+
+def get_city(latitude, longitude, api_key):
+    """
+    Function to get the city name using reverse geocoding from Bing Maps API.
+
+    Parameters:
+    - latitude: Latitude of the location.
+    - longitude: Longitude of the location.
+    - api_key: API key for Bing Maps API.
+
+    Returns:
+    - City name corresponding to the given latitude and longitude.
+    """
+    # Base URL of Bing Maps reverse geocoding API
+    base_url = f'http://dev.virtualearth.net/REST/v1/Locations/{latitude},{longitude}'
+
+    # Parameters for the request
+    params = {
+        'includeNeighborhood': 1,
+        'key': api_key
+    }
+
+    # Make request to Bing Maps reverse geocoding API
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    # Extract city name from the response
+    if 'resourceSets' in data and len(data['resourceSets']) > 0 and 'resources' in data['resourceSets'][0] and len(data['resourceSets'][0]['resources']) > 0:
+        city = data['resourceSets'][0]['resources'][0]['address'].get('locality', '')
+        return city
+    else:
+        return None
+
+def city_dataframe(df1, file_location, api_key):
+    """
+    Function to load or create the city DataFrame.
+
+    If the specified file exists, load the DataFrame from the file.
+    Otherwise, execute the get_city function to obtain the DataFrame and save it to the specified file.
+
+    Parameters:
+    - df1: DataFrame containing the data.
+    - file_location: Location of the file to load or save.
+    - api_key: API key for Bing Maps API.
+
+    Returns:
+    - DataFrame containing city information.
+    """
+    # Check if the specified file exists
+    if os.path.exists(file_location):
+        # If exists, load the DataFrame from the Parquet file
+        df_city = pd.read_parquet(file_location)
+    else:
+        # If not exists, execute get_city function to obtain the DataFrame
+        df_city = df1.copy()  # Copy the original DataFrame
+        df_city['City'] = df_city.apply(lambda row: get_city(row['latitude'], row['longitude'], api_key), axis=1)
+        
+        # Save the DataFrame to the specified file
+        df_city.to_parquet(file_location)
+    
+    return df_city
+
